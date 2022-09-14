6826a1d1fa104c63b28f9f23fdf63adfcf6e7e8e
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-09-14 13:53:25 +0200

    Improve data reporting

--- app.js
+++ app.js
@@ -4,23 +4,32 @@ const PORT = process.env.PORT || 3000;
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-const website =
+const denner =
   "https://www.denner.ch/de/aktionen/list/gf/bier/of/aktionen-aktuelle-woche/";
 
 try {
-  axios(website).then((res) => {
+  let content = [];
+  axios(denner).then((res) => {
     const data = res.data;
     const $ = cheerio.load(data);
 
-    let content = [];
-
     $(".article-item-col", data).each(function () {
       let splitPattern = new RegExp(/\s\s+/g);
-      const price = $(this).text().trim().split(splitPattern);
-      // const price = $(this).text().replace(/\s\s+/g, " ");
-      content.push({
-        price,
+      const sales = $(this).text().trim().split(splitPattern);
+      const ppu = content.push({
+        store: "Denner",
+        item: sales[4],
+        salePrice: sales[1],
+        quantity: sales[5],
+        pricePerUnit: calcPricePerUnit(sales[1], sales[5]),
+        conditions: sales[6] || null,
       });
+
+      function calcPricePerUnit(salePrice, quantity) {
+        let price = Number(salePrice);
+        let quant = Number(quantity.slice(0, quantity.indexOf("x")).trim());
+        return (price / quant).toFixed(2);
+      }
 
       app.get("/", (req, res) => {
         res.json(content);
