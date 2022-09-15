21c1f8e5059d69ba4ad7d0ace41240f15ad131a9
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-09-15 14:48:27 +0200

    Attempt to break the scraping into functions

--- app.js
+++ app.js
@@ -14,28 +14,34 @@ const alloboissons = "https://www.alloboissons.ch/fra/products/promotions";
 const coop =
   "https://www.coop.ch/en/promotions/weekly-special-offers/special-offers-drinks/special-offers-in-beer/c/m_1809?q=%3AhighlightsKW&sort=highlightsKW&pageSize=120"; // lol that page size
 
-try {
+function scrapeDenner() {
   let content = [];
+  try {
+    axios(denner).then((res) => {
+      const data = res.data;
+      const $ = cheerio.load(data);
 
-  /*  Fetching data from Denner */
-  axios(denner).then((res) => {
-    const data = res.data;
-    const $ = cheerio.load(data);
-
-    $(".article-item-col", data).each(function () {
-      let splitPattern = new RegExp(/\s\s+/g);
-      const sales = $(this).text().trim().split(splitPattern);
-      content.push({
-        store: "Denner",
-        percentage: sales[0],
-        item: sales[4],
-        salePrice: sales[1],
-        quantity: sales[5],
-        pricePerUnit: calcPricePerUnit(sales[1], sales[5]),
-        conditions: sales[6] || null,
+      $(".article-item-col", data).each(function () {
+        let splitPattern = new RegExp(/\s\s+/g);
+        const sales = $(this).text().trim().split(splitPattern);
+        content.push({
+          store: "Denner",
+          percentage: sales[0],
+          item: sales[4],
+          salePrice: sales[1],
+          quantity: sales[5],
+          pricePerUnit: calcPricePerUnit(sales[1], sales[5]),
+          conditions: sales[6] || null,
+        });
       });
+      return content;
+    });
+  } catch (err) {
+    console.log(error, error.message);
+  }
+}
 
-      /* Fetching data from Alloboissons 
+/* Fetching data from Alloboissons 
 
       axios(alloboissons).then((res) => {
         const data = res.data;
@@ -48,25 +54,44 @@ try {
       });
 */
 
-      /*  Fetching data from Coop */
+/*  Fetching data from Coop */
+function scrapeCoop() {
+  try {
+    axios(coop).then((res) => {
+      const data = res.data;
+      const $ = cheerio.load(data);
 
-      /* Helper function */
-
-      function calcPricePerUnit(salePrice, quantity) {
-        let price = Number(salePrice);
-        let quant = Number(quantity.slice(0, quantity.indexOf("x")).trim());
-        return (price / quant).toFixed(2);
-      }
-
-      app.get("/", (req, res) => {
-        res.json(content);
+      $(".productTile__price-text-saving-inner", data).each(function () {
+        const salePrice = $(this).text();
+        content.push({
+          store: "Coop",
+          price: price,
+        });
       });
     });
-  });
-} catch (error) {
-  console.log(error, error.message);
+  } catch (error) {
+    console.log(error, error.message);
+  }
 }
+
+app.get("/", async (req, res) => {
+  let dennerData;
+  try {
+    await dennerData = scrapeDenner();
+  } catch (error) {
+    console.log(error);
+  }
+  res.json(dennerData);
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT:${PORT}`);
 });
+
+/* Helper function */
+
+function calcPricePerUnit(salePrice, quantity) {
+  let price = Number(salePrice);
+  let quant = Number(quantity.slice(0, quantity.indexOf("x")).trim());
+  return (price / quant).toFixed(2);
+}
