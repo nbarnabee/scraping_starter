b231195083c1208f5937d878ffc04b5b8eccf9c1
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-09-15 11:17:10 +0200

    Add comments and partial Alloboissons fetch

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const PORT = process.env.PORT || 3000;
 const cheerio = require("cheerio");
 const axios = require("axios");
 
+// For Lidl I would need to use Puppeteer argh
+
 const denner =
   "https://www.denner.ch/de/aktionen/list/gf/bier/of/aktionen-aktuelle-woche/";
 
+const alloboissons = "https://www.alloboissons.ch/fra/products/promotions";
+
+const coop =
+  "https://www.coop.ch/en/promotions/weekly-special-offers/special-offers-drinks/special-offers-in-beer/c/m_1809?q=%3AhighlightsKW&sort=highlightsKW&pageSize=120"; // lol that page size
+
 try {
   let content = [];
+
+  /*  Fetching data from Denner */
   axios(denner).then((res) => {
     const data = res.data;
     const $ = cheerio.load(data);
@@ -16,14 +25,32 @@ try {
     $(".article-item-col", data).each(function () {
       let splitPattern = new RegExp(/\s\s+/g);
       const sales = $(this).text().trim().split(splitPattern);
-      const ppu = content.push({
+      content.push({
         store: "Denner",
+        percentage: sales[0],
         item: sales[4],
         salePrice: sales[1],
         quantity: sales[5],
         pricePerUnit: calcPricePerUnit(sales[1], sales[5]),
         conditions: sales[6] || null,
       });
+
+      /* Fetching data from Alloboissons 
+
+      axios(alloboissons).then((res) => {
+        const data = res.data;
+        const $ = cheerio.load(data);
+
+        $(".product-title", data).each(function () {
+          const title = $(this).text();
+          console.log(title);
+        });
+      });
+*/
+
+      /*  Fetching data from Coop */
+
+      /* Helper function */
 
       function calcPricePerUnit(salePrice, quantity) {
         let price = Number(salePrice);
